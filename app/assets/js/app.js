bf2ac23957faa210da37cde910d7395fa444cbce
Ville Vanninen <ville@workflower.fi>
2016-02-09 22:55:46 +0200

    Add basic keyboard selection

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,14 +1,16 @@
 app = {};
 
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener('DOMContentLoaded', function(event) {
 
   //TODO: First load default document from somewhere, now it's hardcoded in index.html
 
   //Set up initial app state
   var state = {
+    'mode': 'document',
     'sel': {
       'row': '#_0',
-      'elm': document.querySelector('#_0 > :first-child + *')
+      'elm': document.querySelector('#_0 > :first-child')
+      //'farthest': 0, // TODO: So you don't reset your column even if you go through a few rows that have only one column or so
     }
   };
   state.sel.elm.contentEditable = true;
@@ -16,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.execCommand('selectAll',false,null);
 
   //TODO: copy input events from elemental-5
-  document.addEventListener("click", function(event) {
+  document.addEventListener('click', function(event) {
     var elm = event.target;
     var type = elm.nodeName;
     if (type == 'B' || type == 'I' || type == 'Q' || type == 'SPAN') {
@@ -24,6 +26,28 @@ document.addEventListener("DOMContentLoaded", function(event) {
     } else if (elm.classList.contains('row')) {
       state.sel = app.selectElm(elm.querySelector(':first-child + *'), state.sel);
     }
-    console.log(state.sel)
+    console.log(state.sel);
+  })
+  document.addEventListener('keydown', function(event) {
+    var mode = state.mode;
+    var selection = state.sel;
+    var key = keysight(event).key;
+
+    if (key === 'left' && mode === 'document') {
+      state.sel = app.selectPrev(selection);
+      event.preventDefault();
+    }
+    if (key === 'right' && mode === 'document') {
+      state.sel = app.selectNext(selection);
+      event.preventDefault();
+    }
+    if (key === 'up' && mode === 'document') {
+      state.sel = app.selectUp(selection);
+      event.preventDefault();
+    }
+    if (key === 'down' && mode === 'document') {
+      state.sel = app.selectDown(selection);
+      event.preventDefault();
+    }
   })
 });
--- app/assets/js/selection.js
+++ app/assets/js/selection.js
@@ -1,3 +1,4 @@
+//takes new element to select and current selection, returns new selection
 app.selectElm = function select (elm, sel) {
   sel.elm.contentEditable = false;
   elm.contentEditable = true;
@@ -6,15 +7,51 @@ app.selectElm = function select (elm, sel) {
   return {'elm': elm, row: elm.parentElement.id};
 }
 
-app.selectUp = function select (sel) {
-  //TODO: find what needs to be selected, then use app.selectElm to select it and return its return
-}
-app.selectDown = function select (sel) {
-  //TODO: ditto for this and others as in selectUp
-}
 app.selectNext = function select (sel) {
-
+  var newElm = sel.elm;
+  var $newElm = $(sel.elm).next();
+  if ($newElm.length !== 0) {
+    newElm = $newElm[0];
+  }
+  return app.selectElm(newElm, sel);
 }
 app.selectPrev = function select (sel) {
+  var newElm = sel.elm;
+  var $newElm = $(sel.elm).prev();
+  if ($newElm.length !== 0) {
+    newElm = $newElm[0];
+  }
+  return app.selectElm(newElm, sel);
+}
+app.selectUp = function select (sel) {
+  var newElm = sel.elm;
+  var $newElm = $(sel.elm).parent().prev();
+  var index = $(sel.elm).index();
+  var children;
 
+  if ($newElm.length !== 0) {
+    $children = $newElm.children();
+    if ($children.length > index) {
+      newElm = $children[index];
+    } else if ($newElm.length !== 0) {
+      newElm = $children[$newElm.length-1];
+    }
+  }
+  return app.selectElm(newElm, sel);
+}
+app.selectDown = function select (sel) {
+  var newElm = sel.elm;
+  var $newElm = $(sel.elm).parent().next();
+  var index = $(sel.elm).index();
+  var children;
+
+  if ($newElm.length !== 0) {
+    $children = $newElm.children();
+    if ($children.length > index) {
+      newElm = $children[index];
+    } else if ($newElm.length !== 0) {
+      newElm = $children[$newElm.length-1];
+    }
+  }
+  return app.selectElm(newElm, sel);
 }
