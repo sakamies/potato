ec513d5e6f550df0efbe40b79f089f2d43d4d7be
Ville Vanninen <ville@workflower.fi>
2016-03-20 01:22:05 +0200

    fix the logic for determining prop type based on text input of first or last char

--- app/assets/js/input.keyboard.js
+++ app/assets/js/input.keyboard.js
@@ -30,43 +30,38 @@ APP.input.keydown = function (event) {
 
 APP.input.textInput = function (event) {
   //TODO: use getContext here too so the important bits can be scoped to editing a props text
-  var sel = APP.selection;
-  var selElmType = APP.doc.prop.getType(sel.elm);
-  //var whitelist = /[a-z0-9!"#$%&'()*+,.\/:;<=>?@\[\] ^_`{|}~-]/; //TODO: whitelisted or blacklisted characters should be based on language definition
-  //Like blacklisting startsWith & endsWith characters inside a prop, then highlighting them
-  var textContent = sel.elm.textContent;
-  var textSel = window.getSelection();
-  var textRange = textSel.getRangeAt(0);
-  var allSelected = sel.elm.textContent.length === textRange.startOffset + textRange.endOffset
+  let sel = APP.selection;
+  let types = APP.language.types;
+  let selElmType = APP.doc.prop.getType(sel.elm);
+  let textContent = sel.elm.textContent;
+  let textSel = window.getSelection();
+  let textRange = textSel.getRangeAt(0);
+  let allSelected = sel.elm.textContent.length === textRange.startOffset + textRange.endOffset
 
-  sel = APP.doc.prop.validate(sel);
+  //sel = APP.doc.prop.validate(sel);
 
-  //if prop is of default type, try figuring out what type the user wants by looking at the first typed character(s?)
-  if (textContent.length < 3 && selElmType === 0) {
-    for (var i = 0; i < APP.language.types.length; i++) {
-      if (APP.language.types[i].startsWith.indexOf(textContent) !== -1) {
-        sel = APP.doc.prop.init(sel);
-        sel = APP.doc.prop.setType(sel, i);
-        APP.selection = sel;
-        return;
+  //try figuring out what type the user wants by looking at the first typed character
+  if (textContent.length === 1) {
+    let char = textContent;
+    for (let key in types) {
+      let type = types[key];
+      let prevTypeName = APP.doc.prop.getType(sel.elm.previousSibling);
+      if (type.startsWith.indexOf(char) !== -1 && (type.prev.indexOf(prevTypeName) !== -1 || type.prev === 'any')) {
+        sel = APP.doc.prop.init(sel, type);
       }
     }
   }
   //if the prop has some content, check the last char of the prop to determine if the user wants to start a new prop of some type
   else if (textContent.length > 1) {
-    var lastChar = textContent.substring(textContent.length - 1);
-    var nextType = APP.language.types[selElmType].endsWith.indexOf(lastChar);
-
-    if (nextType !== -1) {
-      nextType = APP.language.types[selElmType].next[nextType];
-      for (var i = 0; i < APP.language.types.length; i++) {
-        if (APP.language.types[i].name === nextType) {
-          sel.elm.innerHTML = textContent.substr(0, textContent.length - 1);
-          sel = APP.doc.prop.new(sel, i);
-          APP.selection = sel;
-          return;
-        }
-      }
+    let char = textContent.substring(textContent.length - 1);
+    let nextTypeName = types[selElmType].endsWith.indexOf(char);
+    nextTypeName = types[selElmType].next[nextTypeName];
+    if (nextTypeName in types) {
+      sel.elm.innerHTML = textContent.substr(0, textContent.length - 1);
+      sel = APP.doc.prop.new(sel, nextTypeName);
+      APP.selection = sel;
     }
   }
+
+  APP.selection = sel;
 }
